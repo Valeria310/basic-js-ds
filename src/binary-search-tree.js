655e21f792003e2f14310c8eda389a4a144c6615
Valeria310 <78411807+Valeria310@users.noreply.github.com>
2021-10-13 04:06:11 -0300

    feat: add function

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,9 +8,10 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
+  node=null
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.node
   }
 
   add(/* data */) {

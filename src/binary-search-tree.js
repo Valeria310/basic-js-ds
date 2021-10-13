8c831260655e7f730f32fd36a59e3276e7f35587
Valeria310 <78411807+Valeria310@users.noreply.github.com>
2021-10-13 05:31:05 -0300

    feat: add function

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -32,9 +32,21 @@ module.exports = class BinarySearchTree {
     this.node=addData(this.node, data)
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    function findNode(node, data) {
+      if(node===null) {
+        return false
+      }
+      if(node.data===data) {
+        return true
+      }
+      if(data<node.data) {
+        return findNode(node.left, data)
+      } else {
+        return findNode(node.right, data)
+      }
+    }
+    return findNode(this.node, data);
   }
 
   find(/* data */) {
